ad104a1d356eb9d49412e72d54434d2ac2f3591d
Mustafa Kurukamci <isbuan51@gmail.com>
2020-08-14 11:02:21 +0200

    Updates

---
+++ precache-manifest.d987c67ee584dcabe21e51d483723aac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bd8a4654aed17def2755ae9c3bee395",
+    "url": "/Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "e7f6f50e910fdb12944e",
+    "url": "/Tic-Tac-Toe/static/css/main.5d40d1a2.chunk.css"
+  },
+  {
+    "revision": "db7511ef2fcc00df4587",
+    "url": "/Tic-Tac-Toe/static/js/2.4fd1cbe3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tic-Tac-Toe/static/js/2.4fd1cbe3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7f6f50e910fdb12944e",
+    "url": "/Tic-Tac-Toe/static/js/main.42ed2a36.chunk.js"
+  },
+  {
+    "revision": "e52aaa505ba37bccc14a",
+    "url": "/Tic-Tac-Toe/static/js/runtime-main.db1ab52c.js"
+  }
+]);
